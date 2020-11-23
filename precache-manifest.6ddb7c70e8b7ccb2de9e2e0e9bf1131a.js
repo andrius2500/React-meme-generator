36ef464654a210ee7a0a2aea68b316badcda674f
Andrius <andrius2500@gmail.com>
2020-11-23 17:35:12 +0200

    Updates

---
+++ precache-manifest.6ddb7c70e8b7ccb2de9e2e0e9bf1131a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0188d32df7e63ecf099ffa565bfeeebc",
+    "url": "/React-meme-generator/index.html"
+  },
+  {
+    "revision": "dd2e6ba8ea007aa6c41e",
+    "url": "/React-meme-generator/static/css/main.c162b61f.chunk.css"
+  },
+  {
+    "revision": "4831f0b1bfc17b3266e8",
+    "url": "/React-meme-generator/static/js/2.02143386.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-meme-generator/static/js/2.02143386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2e6ba8ea007aa6c41e",
+    "url": "/React-meme-generator/static/js/main.afd78586.chunk.js"
+  },
+  {
+    "revision": "43d2116fa6ffae0bab94",
+    "url": "/React-meme-generator/static/js/runtime-main.e22b1798.js"
+  }
+]);
